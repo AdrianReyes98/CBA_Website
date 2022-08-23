656bdff47784dce23bd41857ae3593a0bbb882d9
PaolaVelastegui0401 <paovelastegui@hotmail.com>
2022-08-23 13:17:14 -0500

    Commit AGST 23 13:17pm

--- src/app/Views/register/register.component.ts
+++ src/app/Views/register/register.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { User } from 'src/app/Models/User';
+import { ApiUsersService } from 'src/app/Services/api-users.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,10 +12,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-  public isLoading: boolean = false;
+  
+  formUser: any;
+  hide = true;
+  id: number = 0;
   
   constructor(
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
@@ -20,5 +29,19 @@ export class RegisterComponent implements OnInit {
 
   returnLogin(){
     this.router.navigate(['/login'])
+  }
+
+
+  buildFormAdd(){
+    this.formUser = this.formBuilder.group({
+      email: ['',[Validators.required,Validators.email]],
+      user: ['',[Validators.required]],
+      name: ['',[Validators.required]],
+      password: ['',[Validators.required]],
+      address: [''],
+      phone: ['',[Validators.maxLength(10),Validators.minLength(10), Validators.required]],
+      role: ['',Validators.required],
+      identification: ['',[Validators.maxLength(10),Validators.minLength(10)]]
+    });
   }
 }
