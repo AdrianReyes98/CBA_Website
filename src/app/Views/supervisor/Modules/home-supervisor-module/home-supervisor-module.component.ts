afcb0dc4b1b699daa6e55d5d999b127b1e9bc0c8
PaolaVelastegui0401 <paovelastegui@hotmail.com>
2022-08-30 11:34:20 -0500

    Commit AGST 30 11:34am

---
+++ src/app/Views/supervisor/Modules/home-supervisor-module/home-supervisor-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-supervisor-module',
+  templateUrl: './home-supervisor-module.component.html',
+  styleUrls: ['./home-supervisor-module.component.scss']
+})
+export class HomeSupervisorModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/Views/supervisor/supervisor.component.ts
+++ src/app/Views/supervisor/supervisor.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiLoginService } from 'src/app/Services/api-login.service';
 
 @Component({
   selector: 'app-supervisor',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./supervisor.component.scss']
 })
 export class SupervisorComponent implements OnInit {
-
-  constructor() { }
+  user: any;
+  public username: string = "";
+  rol: any
+  public role: string = "";
+  constructor(
+    private apiUserLogin: ApiLoginService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user')!);
+    this.role = JSON.parse(localStorage.getItem('role')!)
+    this.username = this.user.email.toUpperCase();
+    this.rol = this.user.role;
   }
 
+  logOut(): void{
+    this.apiUserLogin.logOut();
+    this.router.navigate(['/login'])
+  }
 }
