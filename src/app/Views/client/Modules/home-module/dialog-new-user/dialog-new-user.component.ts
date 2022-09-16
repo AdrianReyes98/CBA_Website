3b1708ff09455e344b51595bbbfd7c2c6a8e91bb
PaolaVelastegui0401 <paovelastegui@hotmail.com>
2022-09-16 12:26:27 -0500

    Commit 16 SEPT 12:26pm

---
+++ src/app/Views/client/Modules/home-module/dialog-new-user/dialog-new-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-new-user',
+  templateUrl: './dialog-new-user.component.html',
+  styleUrls: ['./dialog-new-user.component.scss']
+})
+export class DialogNewUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
