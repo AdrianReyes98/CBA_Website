7b57a54b0ea647135195b27a96467a73ae122a26
AdrianReyes98 <areyes3804@uta.edu.ec>
2022-09-27 12:31:36 -0500

    Commit 27 SEP 12:31 PM

--- src/app/Views/client/Modules/flow-chart/flow-chart.component.ts
+++ src/app/Views/client/Modules/flow-chart/flow-chart.component.ts
@@ -8,7 +8,9 @@ import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 export class FlowChartComponent implements OnInit {
   
   public listHistory: any[] = [{Accion: 'Ingreso',Descripcion: 'Ingresooo',Fecha: '2022-09-21'},
-                                {Accion: 'Finalizado',Descripcion: 'Finalizado', Fecha: '2022-09-21'}];
+                              {Accion: 'Finalizado',Descripcion: 'Finalizado', Fecha: '2022-09-21'},
+                              {Accion: 'Finalizado',Descripcion: 'Finalizado', Fecha: '2022-09-21'},
+                              {Accion: 'Finalizado',Descripcion: 'Finalizado', Fecha: '2022-09-21'}];
 
   
   
