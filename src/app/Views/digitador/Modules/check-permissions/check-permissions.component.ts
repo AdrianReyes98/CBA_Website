9ca73ca1a63c816ca1f81b0859b46a88ade86e73
PaolaVelastegui0401 <paovelastegui@hotmail.com>
2022-09-28 11:34:21 -0500

    Commit 28 SEPT 11:34am

--- src/app/Views/digitador/Modules/check-permissions/check-permissions.component.ts
+++ src/app/Views/digitador/Modules/check-permissions/check-permissions.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from 'src/app/Views/Common/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-check-permissions',
@@ -8,18 +11,58 @@ import { Component, OnInit } from '@angular/core';
 export class CheckPermissionsComponent implements OnInit {
   protected permission: any;
   
+  
   constructor(
+    public confirmDialog: MatDialog
   ) { }
 
   ngOnInit(): void {
   this.loadData();
     
   }
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow | undefined;
+  public lati:any;
+  public long: any;
 
+  center: google.maps.LatLngLiteral = {
+    lat: -1.25,
+    lng:  -78.60745217922444
+};
+
+zoom = 15;
+  markerOptions: google.maps.MarkerOptions={
+    draggable: false
+  };
+
+  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  openInfoWindow(marker: MapMarker) {
+    if (this.infoWindow != undefined) this.infoWindow.open(marker); 
+  }
+
+  
+
+  acceptPermission(){
+    const dialogRef = this.confirmDialog.open(ConfirmDialogComponent, {
+      width: '410px',
+      data: {title: 'ACEPTAR PERMISO', message: 'Esta seguro que desea aceptar este permiso ?'}
+    });
+
+    dialogRef.afterClosed().subscribe( result => {
+      if(result){
+
+      }
+    });
+  }
 
   loadData(){
     this.permission=JSON.parse(localStorage.getItem('permission')!);
+    this.markerPositions.push({lat: this.permission.idLocNavigation.coordernadaX, lng: this.permission.idLocNavigation.coordenadaY});
     //localStorage.removeItem('permission');
+    this.center = {
+      lat:  this.permission.idLocNavigation.coordernadaX,
+      lng: this.permission.idLocNavigation.coordenadaY
+    }
     console.log(this.permission)
   }
 }
