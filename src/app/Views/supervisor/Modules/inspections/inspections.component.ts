bf7bcdd4a9dad3e860d5a127edd4b4e819f8c13c
PaolaVelastegui0401 <paovelastegui@hotmail.com>
2022-10-05 10:34:51 -0500

    Commit 5 OCT 10:34 AM

--- src/app/Views/supervisor/Modules/inspections/inspections.component.ts
+++ src/app/Views/supervisor/Modules/inspections/inspections.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-inspections',
   templateUrl: './inspections.component.html',
   styleUrls: ['./inspections.component.scss']
 })
 export class InspectionsComponent implements OnInit {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      console.log(this.todo)
+    console.log(this.done)
+    }
+  }
 
   public isLoading: boolean = false;
   protected search: string = "";
@@ -17,6 +34,9 @@ export class InspectionsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    console.log(this.todo)
+    console.log(this.done)
   }
 
   startSearch(){
