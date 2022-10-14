b6a29e9cd7222bffdc012b3c5b920fcabac61420
AdrianReyes98 <areyes3804@uta.edu.ec>
2022-10-14 08:32:52 -0500

    Commit 14 OCT 8:32 AM

--- src/app/Views/supervisor/Modules/inspections/inspections.component.ts
+++ src/app/Views/supervisor/Modules/inspections/inspections.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import * as moment from 'moment';
 
 
 @Component({
@@ -10,42 +10,104 @@ import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag
 export class InspectionsComponent implements OnInit {
 
   
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+  weekDays: any = [
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo"
+  ];
 
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
-  drop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
-      console.log(this.todo)
-    console.log(this.done)
+  monthSelect: any[] = [];
+  dateSelect: any;
+  dateValue: any;
+  currentDate: Date = new Date();
+
+  ngOnInit(){
+    this.getDaysFromDate(this.currentDate.getMonth()+1 , this.currentDate.getFullYear())
+  }
+
+  getDaysFromDate(month: any, year: any) {
+
+    const startDate = moment.utc(`${year}/${month}/01`)
+    const endDate = startDate.clone().endOf('month')
+    this.dateSelect = startDate;
+
+    const diffDays = endDate.diff(startDate, 'days', true)
+    const numberDays = Math.round(diffDays);
+
+    const arrayDays = Object.keys([...Array(numberDays)]).map((a: any) => {
+      a = parseInt(a) + 1;
+      const dayObject = moment(`${year}-${month}-${a}`);
+      return {
+        name: dayObject.format("dddd"),
+        value: a,
+        indexWeek: dayObject.isoWeekday()
+      };
+    });
+
+    this.monthSelect = arrayDays;
+  }
+
+  changeMonth(flag: any) {
+
+    const month = parseInt(this.dateSelect.clone().format('MM') + flag);
+    const year = this.dateSelect.clone().format('YYYY');
+
+    if(month > this.currentDate.getMonth()+1 || year != this.currentDate.getFullYear()){
+      if (flag < 0) {
+        const prevDate = this.dateSelect.clone().subtract(1, "month");
+        this.getDaysFromDate(prevDate.format("MM"), prevDate.format("YYYY"));
+      } else {
+        const nextDate = this.dateSelect.clone().add(1, "month");
+        this.getDaysFromDate(nextDate.format("MM"), nextDate.format("YYYY"));
+      }
+    } 
+
+  }
+
+  colorDay(day: any){
+    const month = this.dateSelect.clone().format('MM');
+    const currentDay = this.currentDate.getDate();
+    const year = parseInt(this.dateSelect.clone().format('YYYY'));
+
+    if(currentDay == day.value && month == this.currentDate.getMonth()+1 && year == this.currentDate.getFullYear()){
+      return '#fff'
+    }else{
+      if(currentDay > day.value && month ==  this.currentDate.getMonth()+1 && year == this.currentDate.getFullYear()){
+        return 'gray'
+      }else{
+        return '#000'
+      }
     }
   }
 
-  public isLoading: boolean = false;
-  protected search: string = "";
-  protected dataSource: any;
-  public columns: string[] = ['Id','Cedula','Role', 'Email','Acciones'];
-  
-  constructor() { }
+  backgroundDay(day: any){
+    const currentDay = this.currentDate.getDate();
+    const month = parseInt(this.dateSelect.clone().format('MM'));
+    const year = parseInt(this.dateSelect.clone().format('YYYY'));
 
-  ngOnInit(): void {
+    if(currentDay == day.value && month == this.currentDate.getMonth()+1 && year == this.currentDate.getFullYear()){
+      return '#8C0C0C'
+    }
 
-    console.log(this.todo)
-    console.log(this.done)
+    return ''
   }
 
-  startSearch(){
-  }
+  clickDay(day: any) {
+    const month = parseInt(this.dateSelect.clone().format('MM'));
+    const currentDay = this.currentDate.getDate();
+    const year = parseInt(this.dateSelect.clone().format('YYYY'));
 
-  openDialogAddUser(){
+    if(!(currentDay >= day.value) && ( month <= this.currentDate.getMonth()+1 && year >= this.currentDate.getFullYear())){
+      console.log('SI');
+      const monthYear = this.dateSelect.format('YYYY-MM')
+      const parse = `${monthYear}-${day.value}`
+      const objectDate = moment(parse)
+      this.dateValue = objectDate;
+    }
   }
-
 }
